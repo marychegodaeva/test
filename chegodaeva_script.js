d261597073156b20f709b1b611c42a19011c0353
vdghcyram <marychegodaeva@gmail.com>
2025-06-22 19:42:19 +0300

    Операторы

--- chegodaeva_script.js
+++ chegodaeva_script.js
@@ -1,24 +1,36 @@
-let typeString = 'javascript'
-let typeNumber = 25
-let typeBoolean = true
-let typeUndefined = undefined
-let typeNull = null
-let typeArray = [2, 4, 6] 
-let typeObject = {name: 'Name'}
-let typeSymbol = Symbol("id")
+//1
+let n1 = 2 * 2 + 2
+let n2 = (Math.sin(54) * Math.cos(16)) ** 2
+let n3 = (((16 * Math.sqrt(13.2 * 71.9)) / (2.4 / (7 ** 4))) + (3 ** Math.sqrt(49))) * (2 ** 7)
 
-console.log(typeof typeString); // "string"
-console.log(typeof typeNumber); // "number"
-console.log(typeof typeBoolean); // "boolean"
-console.log(typeof typeUndefined); // "undefined"
-console.log(typeof typeNull); // "object"
-console.log(typeof typeArray); // "object"
-console.log(typeof typeObject); // "object"
-console.log(typeof typeSymbol); // "symbol"
+console.log('n1 =', Math.round(n1)) // 6
+console.log('n2 =', Math.round(n2)) // 0.2863637210469634 ~ 0
+console.log('n3 =', Math.round(n3)) // 63399251.636757076 ~ 63399252
 
 //2
-/*
-  Объяснение ссылочных типов данных простыми словами.
-  У меня есть страница GitHub, куда я отправляю коммиты с изменениями кода. Если я отправлю ссылку ментору1, он будет видеть все изменения, которые я вношу. Если я отправлю ссылку ментору2, он также будет видеть. Если ментор1 отправит эту же ссылку ментору2, они оба будут наблюдать коммиты. Если я добавлю новый коммит, то у обоих менторов обновится информация по ссылке.
-  Если ментор2 внесет свои изменения в мой проект (обусловимся, что у нас общий доступ к проекту), ментор1 и я увидим изменения.
-*/
+let num1 = 4;
+let num2 = 7;
+let num3 = 10;
+let num4 = 15;
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+console.log(isEven(num1)); // true
+console.log(isEven(num2)); // false
+console.log(isEven(num3)); // true
+console.log(isEven(num4)); // false
+
+//3
+function greet(name) {
+  if (!name) {
+    console.log("Hello, Guest!");
+  } else {
+    console.log("Hello, " + name + "!");
+  }
+}
+
+greet(); // Hello, Guest!
+greet("Ivan"); // Hello, Алексей!
+greet(""); // Hello, Guest!
