207e66d89ca034de4d93f39b7e8110578bff7123
vdghcyram <marychegodaeva@gmail.com>
2025-06-22 14:40:52 +0300

    clear files

--- chegodaeva_script.js
+++
@@ -1,7 +0,0 @@
-let city = "Москва"; // мой город
-let temp = 18; // температура воздуха в моем городе
-console.log("Город: ", city);
-console.log("Температура воздуха: ", temp);
-
-let expression = (10 + 15) * 3; // выражение
-console.log(expression);
