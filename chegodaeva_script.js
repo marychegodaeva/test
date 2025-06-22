12a38f5551b3ff0b12aa0801d70e7251251f71a7
vdghcyram <marychegodaeva@gmail.com>
2025-06-22 14:31:38 +0300

    test commit

---
+++ chegodaeva_script.js
@@ -0,0 +1,7 @@
+let city = "Москва"; // мой город
+let temp = 18; // температура воздуха в моем городе
+console.log("Город: ", city);
+console.log("Температура воздуха: ", temp);
+
+let expression = (10 + 15) * 3; // выражение
+console.log(expression);
