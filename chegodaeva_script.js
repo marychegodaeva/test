1e6a18b18e6f237515d53b858b55f3d8310a00f9
vdghcyram <marychegodaeva@gmail.com>
2025-06-23 10:28:09 +0300

    Объекты

--- chegodaeva_script.js
+++ chegodaeva_script.js
@@ -1,35 +1,28 @@
 //1
-function hasEvenNumber(arr) {
-  let foundEven = false;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      foundEven = true;
-      break;
-    } 
-  }
-  return foundEven;
+const person = {
+  name: 'Maria',
+  age: 22,
+  hobbies: ['Спорт', 'Чтение'],
 }
-console.log(hasEvenNumber([1, 3, 4, 5])); // true
+console.log(Object.keys(person)) // [ 'name', 'age', 'hobbies' ]
+console.log(Object.entries(person)) // [ [ 'name', 'Maria' ], [ 'age', 22 ], [ 'hobbies', [ 'Спорт', 'Чтение' ] ] ]
+
+console.log(person.name) // Maria
+console.log(person.age) // 22
+console.log(person.hobbies) // [ 'Спорт', 'Чтение' ]
+
+console.log(person['name']) // Maria
+console.log(person['age']) // 22
+console.log(person['hobbies']) // [ 'Спорт', 'Чтение' ]
 
 //2
-function calculateAverage(numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) { 
-    sum += numbers[i];
-  }
-  return sum / numbers.length;
-}
-console.log(calculateAverage([2, 4, 6])); // 4
+person.work = 'developer'
+console.log(Object.keys(person)) // [ 'name', 'age', 'hobbies', 'work' ]
+console.log(person.work) // developer
 
-//3
-function findLargestNumber(arr) {
-  if (arr.length === 0) return undefined;
-  let largest = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > largest) {
-      largest = arr[i];
-    }
-  }
-  return largest;
-}
-console.log(findLargestNumber([-10, -20, -30])); // -10
+person['age'] = 21
+console.log(person.age) // 21
+console.log(person['age']) // 21
+
+delete person['hobbies']
+console.log(Object.entries(person)) // [ [ 'name', 'Maria' ], [ 'age', 21 ], [ 'work', 'developer' ] ]
