25b402f7848f30e0b596c2a0435162f7711a9467
vdghcyram <marychegodaeva@gmail.com>
2025-06-24 22:25:38 +0300

    this, контекст - 2024

--- chegodaeva_script.js
+++ chegodaeva_script.js
@@ -1,69 +1,23 @@
 //1
-const student = {
-  name: 'Иван',
-  age: 20,
-  courses: ['Математика', 'История', 'Программирование'],
-  address: {
-    city: 'Москва',
-    street: 'Ленинская',
-    house: 10
+const person = {
+  name: "Alice",
+  greet: function() {
+    console.log(`Hello, my name is ${this.name}`);
   }
 };
 
-const shallowCopy1 = { ...student };
-shallowCopy1.courses.push('Физика');
-shallowCopy1.address.city = 'Санкт-Петербург';
-console.log(student); // courses: [ 'Математика', 'История', 'Программирование', 'Физика' ],  address: { city: 'Санкт-Петербург', street: 'Ленинская', house: 10 }
-
-const shallowCopy2 = Object.assign({}, student);
-shallowCopy2.courses.push('Химия');
-shallowCopy2.address.street = 'Невская';
-console.log(student); // courses: [ 'Математика', 'История', 'Программирование', 'Физика', 'Химия' ],  address: { city: 'Санкт-Петербург', street: 'Невская', house: 10 }
+const greetFunction = person.greet;
+greetFunction(); // Hello, my name is undefined
 
 //2
-const user = {
-  name: "Alice",
-  age: 30,
-  address: {
-    city: "Wonderland",
-    zip: "12345"
-  },
-  sayHi: () => console.log('Hello, Alice!')
-};
-
-const jsonCopyUser = JSON.parse(JSON.stringify(user));
-console.log(jsonCopyUser); 
-// {
-//   name: 'Alice',
-//   age: 30,
-//   address: { city: 'Wonderland', zip: '12345' }
-// }
-jsonCopyUser.sayHi?.(); // Ошибка, так как функции не копируются
-
-//3
-function deepCopy(obj) {
-  if (obj === null || typeof obj !== 'object') {
-    return obj;
-  }
-
-  if (Array.isArray(obj)) {
-    const arrCopy = [];
-    for (let i = 0; i < obj.length; i++) {
-      arrCopy[i] = deepCopy(obj[i]);
-    }
-    return arrCopy;
-  }
-
-  const objCopy = {};
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      objCopy[key] = deepCopy(obj[key]);
-    }
-  }
-  return objCopy;
+function greet(language, punctuation) {
+  console.log(`${language}, ${this.name}${punctuation}`);
 }
 
-const deepCopyUser = deepCopy(user);
-deepCopyUser.address.city = "New Wonderland";
-console.log(user.address.city); // Wonderland
-console.log(deepCopyUser.address.city); // New Wonderland
+const person1 = { name: "Alice" };
+const person2 = { name: "Bob" };
+
+greet.call(person1, "Hello", "!"); // Hello, Alice!
+greet.apply(person2, ["Hi", "!!"]); // Hi, Bob!!
+const greetAlice = greet.bind(person1, "Hey");
+greetAlice("..."); // Hey, Alice...
