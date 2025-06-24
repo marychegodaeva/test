851c14d8df5ecae1e81d442b25d4133cd2ad910e
vdghcyram <marychegodaeva@gmail.com>
2025-06-24 21:55:52 +0300

    Объекты - 2024

--- chegodaeva_script.js
+++ chegodaeva_script.js
@@ -1,23 +1,35 @@
-//2
-const rect = document.getElementById('rect');
+const student = {
+  name: 'Valentin',
+  age: 21,
+  grades: [ 'A', 'B', 'C', 'D' ],
+  address: {
+    city: 'Krasnodar',
+    street: 'Lenina'
+  }
+}
 
-rect.addEventListener('mouseover', () => {
-  rect.style.width = '200px';
-  rect.style.backgroundColor = 'blue';
-  rect.style.transition = '0.5s';
-});
+console.log(student.name) // Valentin
+console.log(student.grades) // [ 'A', 'B', 'C', 'D' ]
+console.log(student.address) // { city: 'Krasnodar', street: 'Lenina' }
 
-rect.addEventListener('mouseout', () => {
-  rect.style.width = '100px';
-  rect.style.backgroundColor = 'green';
-  rect.style.transition = '0.5s';
-});
+console.log(student['age']) // 21
+console.log(student['grades']) // [ 'A', 'B', 'C', 'D' ]
+console.log(student['address']) // { city: 'Krasnodar', street: 'Lenina' }
 
-//4
-const form = document.getElementById('form');
-const successMessage = document.getElementById('successMessage');
+student.isGraduated = '2020'
+console.log(student.isGraduated) // 2020
+console.log(student['isGraduated']) // 2020
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  alert('Форма отправлена!');
-});
+student.name = 'Ivan'
+console.log(student['name']) // Ivan
+console.log(student.name) // Ivan
+
+delete student.address
+console.log(student.address) // undefined
+console.log(student)
+// {
+//   name: 'Ivan',
+//   age: 21,
+//   grades: [ 'A', 'B', 'C', 'D' ],
+//   isGraduated: '2020'
+// }
