ff01d4b02ec24788b8cace4c72c81a2eac706bb7
vdghcyram <marychegodaeva@gmail.com>
2025-07-03 12:14:25 +0300

    this, контекст

--- chegodaeva_script.js
+++ chegodaeva_script.js
@@ -1,24 +1,49 @@
 //1
-const squareNumbers = (numbers) => numbers.map(num => num * num);
-const numbers = [1, 2, 3, 4, 5];
-const squaredNumbers = squareNumbers(numbers);
-console.log(squaredNumbers); // [1, 4, 9, 16, 25]
+const person = {
+  name: "Alice",
+  greet: function () {
+    console.log(`Hello, my name is ${this.name}`);
+  },
+};
+
+const greetFunction = person.greet;
+greetFunction(); // Hello, my name is undefined
 
 //2
-const myObject = {
-  value: "Hello, World!",
-  printValue: function() {
-    setTimeout(() => {
-      console.log(this.value);
-    }, 1000);
+// В первом случае, когда вызывается student.greet(), this ссылается на объект student, 
+// поэтому this.name возвращает 'Alice'.
+// Во втором случае, когда вызывается student.delayedGreet(), this внутри setTimeout теряет контекст и 
+// ссылается на глобальный объект (window), где свойства name не существует, поэтому выводится undefined.
+const student = {
+  name: 'Alice',
+
+  greet: function() {
+    console.log(`Hello, ${this.name}!`);
+  },
+
+  delayedGreet: function() {
+    setTimeout(() => this.greet(), 1000);
   }
 };
 
-myObject.printValue(); // Hello, World!
+student.greet(); // Hello, Alice
+student.delayedGreet(); // Через 1 секунду выведет: Hello, Alice
 
 //3
-const applyFunctionToArray = (func, array) => array.map(item => func(item));
-// const numbers = [1, 2, 3, 4, 5];
-const double = x => x * 2;
-const doubledNumbers = applyFunctionToArray(double, numbers);
-console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+function greet(language, punctuation) {
+  console.log(`${language}, ${this.name}${punctuation}`);
+}
+
+const person1 = { name: "Alice" };
+const person2 = { name: "Bob" };
+
+greet.call(person1, "Hello", "!"); // Hello, Alice!
+greet.apply(person2, ["Hi", "!!"]); // Hi, Bob!!
+const greetAlice = greet.bind(person1, "Hey");
+greetAlice("..."); // Hey, Alice...
+
+//4
+// В результате выполнения sayHelloToAdmin() в консоли будет выведено 'Hello, Bob', 
+// потому что sayHelloToAdmin привязан к объекту admin с помощью bind.
+// const sayHelloToUser = sayHello.bind(user);
+// sayHelloToUser(); // Hello, John
